79c94135c4de0a122b940d29f97bfc9209b65db1
Zach Kern <zkern@productionpulse.com>
2026-06-25 16:26:05 -0400

    added wpm

--- lua/user/plugins/statusline.lua
+++ lua/user/plugins/statusline.lua
@@ -3,7 +3,26 @@ return {
 	dependencies = {"nvim-tree/nvim-web-devicons"},
 	config = function()
 		require("lualine").setup({
-			options = {theme = "auto"},
+			options = {
+				theme = "solarized_dark",
+				section_separators = { left = "", right = "" },
+				component_separators = { left = "", right = "" },
+			},
+			sections = {
+				lualine_x = {
+					{
+						function()
+							return require("wpm-tracker").get_wpm_display()
+						end,
+						cond = function()
+							return require("wpm-tracker").get_current_wpm() > 0
+						end,
+						color = { fg = "#3EFFDC" },
+					},
+					"encoding",
+					"filetype",
+				},
+			}
 		})
 	end,
 }
---
+++ lua/user/plugins/wpm.lua
@@ -0,0 +1,7 @@
+return {
+	"leonardcser/wpm-tracker.nvim",
+	config = function()
+		require("wpm-tracker").setup({
+		})
+	end,
+}
